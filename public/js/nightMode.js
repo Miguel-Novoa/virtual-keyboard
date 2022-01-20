190e246bb1de7511024fb64876f00d8a9b87a45b
Miguel Novoa <novoarodriguezmiguel@gmail.com>
2022-01-20 16:08:42 +0100

    first commit

---
+++ public/js/nightMode.js
@@ -0,0 +1,61 @@
+const switchInput = document.querySelector('#switch');
+const smallKeys = document.querySelectorAll('.smallKey');
+const mediumKeys = document.querySelectorAll('.mediumKey');
+const spacebar = document.querySelector('#spacebar');
+const keyboard = document.querySelector('#keyboard');
+const main = document.querySelector('main');
+const header = document.querySelector('header');
+const switchTitle = document.querySelector('#switchTitle');
+const textZone = document.querySelector('#text');
+
+const switchActivation = () =>{
+    if(switchInput.checked == true){
+        console.log('switch');
+        smallKeys.forEach(function(key){
+            key.classList.remove('smallKeyStyleLight');
+            key.classList.add('smallKeyStyleNight')
+        });
+
+        mediumKeys.forEach(function(key){
+            key.classList.remove('mediumKeyStyleLight');
+            key.classList.add('mediumKeyStyleNight')
+        });
+
+        spacebar.classList.remove('spacebarStyleLight');
+        spacebar.classList.add('spacebarStyleNight');
+
+        keyboard.classList.remove('keyboardStyleLight');
+        keyboard.classList.add('keyboardStyleNight');
+
+        main.classList.add('mainNight');
+        header.classList.add('headerNight');
+        switchTitle.classList.add('switchBtnTitle');
+        textZone.classList.add('textZoneNight');
+        textZone.classList.remove('textZoneLight');
+    }else{
+        console.log('error')
+        smallKeys.forEach(function(key){
+            key.classList.remove('smallKeyStyleNight');
+            key.classList.add('smallKeyStyleLight')
+        });
+
+        mediumKeys.forEach(function(key){
+            key.classList.remove('mediumKeyStyleNight');
+            key.classList.add('mediumKeyStyleLight')
+        });
+
+        spacebar.classList.remove('spacebarStyleNight');
+        spacebar.classList.add('spacebarStyleLight');
+
+        keyboard.classList.remove('keyboardStyleNight');
+        keyboard.classList.add('keyboardStyleLight');
+
+        main.classList.remove('mainNight');
+        header.classList.remove('headerNight');
+        switchTitle.classList.remove('switchBtnTitle');
+        textZone.classList.remove('textZoneNight');
+        textZone.classList.add('textZoneLight');
+    };
+};
+
+switchInput.addEventListener('click', switchActivation);
